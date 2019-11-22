a43d28020839b3c545d7290eb351d9319e96f7d4
Lars Nysom <lars.nysom@gmail.com>
2019-11-22 12:38:26 +0100

    Initial commit

---
+++ ClientManager/ClientManager.DAL/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.DAL
+{
+    public class Customer
+    {
+        public int CustomerId { get; internal set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Changed { get; set; }
+    }
+}
---
+++ ClientManager/ClientManager.DAL/CustomerRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.DAL
+{
+    public class CustomerRepository
+    {
+        public Customer Get(int id)
+        {
+            // TODO: Implement the method so it returns the customer with the specified id.
+
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            // TODO: Implement the method so it return a list of all the customers in the database
+
+            throw new NotImplementedException();
+        }
+
+        public Customer Update(Customer customer)
+        {
+            // TODO: Update the customer in the database with the provided data and return the updated customer object
+
+            throw new NotImplementedException();
+        }
+
+        public Customer Insert(Customer customer)
+        {
+            // TODO: Insert a new customer into the database with the provided data and return the new customer object 
+
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Customer customer)
+        {
+            // TODO: Delete the customer defined in the provided customer object from the database and return a value indicating the success/failure of the operation
+
+            throw new NotImplementedException();
+        }
+    }
+}
