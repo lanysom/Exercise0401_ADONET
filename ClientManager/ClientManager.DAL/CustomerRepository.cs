8244ea1c81cc7885462643e257240c42536b670d
Lars Nysom <lars.nysom@gmail.com>
2019-11-22 12:43:51 +0100

    Updated todos

--- ClientManager/ClientManager.DAL/CustomerRepository.cs
+++ ClientManager/ClientManager.DAL/CustomerRepository.cs
@@ -8,37 +8,37 @@ namespace ClientManager.DAL
 {
     public class CustomerRepository
     {
-        public Customer Get(int id)
+        public IEnumerable<Customer> GetAll()
         {
-            // TODO: Implement the method so it returns the customer with the specified id.
+            // TODO: 1. Implement the method so it return a list of all the customers in the database
 
             throw new NotImplementedException();
         }
 
-        public IEnumerable<Customer> GetAll()
+        public Customer Get(int id)
         {
-            // TODO: Implement the method so it return a list of all the customers in the database
+            // TODO: 2. Implement the method so it returns the customer with the specified id.
 
             throw new NotImplementedException();
         }
 
         public Customer Update(Customer customer)
         {
-            // TODO: Update the customer in the database with the provided data and return the updated customer object
+            // TODO: 3. Update the customer in the database with the provided data and return the updated customer object
 
             throw new NotImplementedException();
         }
 
         public Customer Insert(Customer customer)
         {
-            // TODO: Insert a new customer into the database with the provided data and return the new customer object 
+            // TODO: 4. Insert a new customer into the database with the provided data and return the new customer object 
 
             throw new NotImplementedException();
         }
 
         public bool Delete(Customer customer)
         {
-            // TODO: Delete the customer defined in the provided customer object from the database and return a value indicating the success/failure of the operation
+            // TODO: 5. Delete the customer defined in the provided customer object from the database and return a value indicating the success/failure of the operation
 
             throw new NotImplementedException();
         }
