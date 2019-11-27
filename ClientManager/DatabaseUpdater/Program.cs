c143e29530173a418973b85cbf72390f986bfb47
Lars Nysom <lars.nysom@gmail.com>
2019-11-27 13:50:32 +0100

    commiting database updater

---
+++ ClientManager/DatabaseUpdater/Program.cs
@@ -0,0 +1,38 @@
+﻿using DbUp;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+
+namespace DatabaseUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings[0].ConnectionString;
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+        }
+    }
+}
